0f758f88875b8d3a9d84721a1324306098b24b6f
becca <rebecca.kaci@gmail.com>
2021-11-04 15:12:39 +0100

    définition du state initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      characters: []
+    }
+  }
   render() {
     return (
       <div>
-        
+        <h1>Game of thrones</h1>
       </div>
     );
   }
