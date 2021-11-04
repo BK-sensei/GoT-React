0814ea6141886ad9e1eb3c401d71aa5dfd9a20fa
becca <rebecca.kaci@gmail.com>
2021-11-04 17:02:59 +0100

    ça brille

--- src/components/Character.js
+++ src/components/Character.js
@@ -4,8 +4,8 @@ class Character extends React.Component {
 	render() {
 		return(
 			<div className= "col-xl-4 col-md-6 col-sm-12 mb-4">
-                <div className="card">
-                    <img className="card-img-top p-1" src={this.props.image} alt="GoT's character"/>
+                <div className="card characterGot">
+                    <div className="card-img-top img-fluid p-1 characterGot-img" style={{backgroundImage: `url(${this.props.image})`}} alt="GoT's character"></div>
                     <div className="card-body">
                         <h5 className="card-title text-center fw-bold">{this.props.name}</h5>
                         <p className="card-text text-center">{this.props.title}</p>
